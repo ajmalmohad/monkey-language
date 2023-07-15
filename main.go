ce11920e47447de4d92cad9bd8ef28f76a61995c
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-07-15 23:00:40 +0530

    REPL for Lexing

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+	"strings"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nHello %s! Welcome to Monkey 1.0\n", strings.Split(user.Username, "\\")[0])
+	fmt.Printf("Feel Free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
--- repl/repl.go
+++ repl/repl.go
@@ -1,1 +1,29 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		input := scanner.Text()
+		lex := lexer.CreateLexer(input)
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
