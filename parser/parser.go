fd91ed811c9dde67ada02bdfb042e929d2bdacb8
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-07-19 14:42:28 +0530

    Parser root node

--- parser/parser.go
+++ parser/parser.go
@@ -19,6 +19,21 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) parseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
 	return nil
 }
 
