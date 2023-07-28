53b0b8922531b06b7acaa6f43054e6d2aaf33533
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-07-28 23:20:47 +0530

    IfElse and BlockStatements

--- ast/ast.go
+++ ast/ast.go
@@ -253,6 +253,68 @@ func (b *Boolean) String() string {
 	return b.Token.Literal
 }
 
-func (ie *Boolean) expressionNode() {
+func (b *Boolean) expressionNode() {
+
+}
+
+/**
+ * * IfExpression
+ */
+
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+func (ie *IfExpression) expressionNode() {
+
+}
+
+/**
+ * * BlockStatement
+ */
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (bs *BlockStatement) expressionNode() {
 
 }
