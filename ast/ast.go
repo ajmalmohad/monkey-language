94f0850e6f07097b9a8c9bcea96ad7df1eebcca6
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-07-29 10:00:25 +0530

    Function Literal

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 type Node interface {
@@ -316,5 +317,40 @@ func (bs *BlockStatement) String() string {
 }
 
 func (bs *BlockStatement) expressionNode() {
+
+}
+
+/**
+ * * FunctionLiteral
+ */
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
+
+func (fl *FunctionLiteral) expressionNode() {
 
 }
