52dfe4d2306bde02effb93a00d97a10583057eeb
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-07-29 10:23:11 +0530

    Call Expressions

--- ast/ast.go
+++ ast/ast.go
@@ -354,3 +354,37 @@ func (fl *FunctionLiteral) String() string {
 func (fl *FunctionLiteral) expressionNode() {
 
 }
+
+/**
+ * * CallExpression
+ */
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (ce *CallExpression) TokenLiteral() string {
+	return ce.Token.Literal
+}
+
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (ce *CallExpression) expressionNode() {
+
+}
