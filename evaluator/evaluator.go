fb25c064fe56bcea40b16e0a977a8047080a889b
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-01 17:20:32 +0530

    Infix Expression Evaluation (Comparision + Operation )

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -20,6 +20,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
@@ -75,5 +79,53 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	} else {
 		value := right.(*object.Integer).Value
 		return &object.Integer{Value: -value}
+	}
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+	switch operator {
+	case "+":
+		value := leftVal + rightVal
+		return &object.Integer{Value: value}
+	case "-":
+		value := leftVal - rightVal
+		return &object.Integer{Value: value}
+	case "*":
+		value := leftVal * rightVal
+		return &object.Integer{Value: value}
+	case "/":
+		value := leftVal / rightVal
+		return &object.Integer{Value: value}
+	case "==":
+		value := leftVal == rightVal
+		return &object.Boolean{Value: value}
+	case "!=":
+		value := leftVal != rightVal
+		return &object.Boolean{Value: value}
+	case ">":
+		value := leftVal > rightVal
+		return &object.Boolean{Value: value}
+	case "<":
+		value := leftVal < rightVal
+		return &object.Boolean{Value: value}
+	case ">=":
+		value := leftVal >= rightVal
+		return &object.Boolean{Value: value}
+	case "<=":
+		value := leftVal <= rightVal
+		return &object.Boolean{Value: value}
+	default:
+		return NULL
 	}
 }
