6e03652345c138112e6fa05086741b9c94b0dfd9
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-01 16:59:14 +0530

    Minus Prefix Expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -49,6 +49,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -64,5 +66,14 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		return TRUE
 	default:
 		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	} else {
+		value := right.(*object.Integer).Value
+		return &object.Integer{Value: -value}
 	}
 }
