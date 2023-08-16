7dc04290e4e9fb89a44db9ef3aedb999a19f02ac
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-16 21:07:06 +0530

    Index Expression AST Node

--- ast/ast.go
+++ ast/ast.go
@@ -441,3 +441,33 @@ func (al *ArrayLiteral) String() string {
 func (al *ArrayLiteral) expressionNode() {
 
 }
+
+/**
+ * * Index Expression
+ */
+
+type IndexExpression struct {
+	Token token.Token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
+
+func (ie *IndexExpression) expressionNode() {
+
+}
