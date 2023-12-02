ee329e7fbe38c013c4f163b9cc1f0fdc8f9aba22
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-12-02 19:51:34 +0530

    Compiler Byte Code

---
+++ Compiler/code/code.go
@@ -0,0 +1,30 @@
+package code
+
+import (
+	"fmt"
+)
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+	return def, nil
+}
+
+type Instructions []byte
+
+type Opcode byte
+
+const (
+	OpConstant Opcode = iota
+)
